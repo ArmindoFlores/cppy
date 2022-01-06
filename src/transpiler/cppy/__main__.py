8b7f836ff9f8dca7b994155412572e02c80781db
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2022-01-06 20:56:42 +0000

    Updated build options

--- src/transpiler/cppy/__main__.py
+++ src/transpiler/cppy/__main__.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+import shlex
 import subprocess
 
 from antlr4 import CommonTokenStream, FileStream
@@ -8,8 +11,19 @@ from .Python3Parser import Python3Parser
 from .Visitor import Visitor
 
 
-def main(filename, outfile):
-    i = FileStream(filename)
+def parse_args():
+    parser = argparse.ArgumentParser(description="A Python to C++ transpiler")
+    parser.add_argument("option", choices=["transpile", "compile", "run"], help="What action to take")
+    parser.add_argument("-o", "--output", help="Executable name", default="main")
+    parser.add_argument("-m", "--mode", choices=["debug", "release"], default="release", help="Compilation mode")
+    parser.add_argument("-lm", "--link-mode", choices=["static", "shared"], default="static", help="Linking mode (for the cppy library)")
+    parser.add_argument("-c", "--compiler", default="g++", help="Compiler name. Defaults to 'g++'")
+    parser.add_argument("-O", "--compiler-options", help="Options to be directly passed to the compiler")
+    parser.add_argument("filename", help="File to be compiled")
+    return parser.parse_args()
+
+def main(args):
+    i = FileStream(args.filename)
     lexer = Python3Lexer(i)
     stream = CommonTokenStream(lexer)
     parser = Python3Parser(stream)
@@ -19,13 +33,35 @@ def main(filename, outfile):
     
     visitor = Visitor()
     visitor.visit(tree)
-
     
-    with open(outfile+".cpp", "w") as f:
+    with open(args.output+".cpp", "w") as f:
         f.write(visitor.getCode())
 
-    subprocess.run(["g++", "-std=c++17", "-ggdb", "-o", outfile, outfile+".cpp", "-I../python", "-L../../build", "-l:libcppy.a"])
+    if args.option == "transpile":
+        return
+
+    try:
+        base_args = [args.compiler, "-std=c++17", "-ggdb" if args.mode == "debug" else "-O3"]
+        if args.mode == "release":
+            base_args.append("-s")
+        base_args.extend(["-o", args.output])
+        base_args.append(args.output+".cpp")
+        base_args.extend(["-I../src/python", "-l:libcppy.a" if args.link_mode == "static" else "-lcppy"])
+        if args.compiler_options:
+            base_args.extend(shlex.split(args.compiler_options))
+        print(base_args)
+        process = subprocess.run(base_args)
+        if process.returncode != 0:
+            print("Failed to build")
+            return
+    finally:
+        os.remove(args.output+".cpp")
+
+    if args.option == "compile":
+        return
+
+    subprocess.run(["./"+args.output])
     
 
 if __name__ == "__main__":
-    main("../../tests/test.py", "test")
+    main(parse_args())
